e9ffb5cc90a733cd1020d70623e34e057894cbcb
Rodrigo Brito <rodrito90@gmail.com>
2019-05-11 02:08:05 -0300

    Atualizando o método da Biseccão com novas funcionalidades e organizando o código

--- Funcoes/Biseccao.py
+++ Funcoes/Biseccao.py
@@ -1,38 +1,111 @@
-# encoding: utf-8
+# -*- coding: utf-8 -*-
 from copy import copy
 from math import log10, ceil, fabs
-	
-def biseccao(intervalo = [0,0], condParada = 0):
-	
-	funcao = "x**3 - 9*x + 3"
-	print("Função =",funcao)
 
+def _fa(a):
+	'''
+	Resultado da função f(a) no ponto A do intervalo [a,b]
+
+	Parametro:\n
+	a = ponto a do intervalo [a,b]
+	'''
+	return a**3 + 9*a - 8
+
+def _fb(b):
+	'''
+	Resultado da função f(b) no ponto B do intervalo [a,b]
+
+	Parametro:\n
+	b = ponto b do intervalo [a,b]
+	'''
+	return b**3 + 9*b - 8
+
+def _fxn(xn):
+	'''
+	Resultado da função f(xn) no ponto medio do intervalo [a,b]
+	
+	Parametro:\n
+	xn = ponto medio entre o intervalo [a,b]
+	'''
+	return xn**3 + 9*xn - 8
+
+def verificaSeTemRaiz(fa, fb):
+	'''
+	Verifica se tem raiz entre duas funções. Se o resultado da multiplicação entre elas for negativo
+	
+	Parametros:\n
+	fa = o resultado de uma função no ponto 'a' do intervalo [a,b]\n
+	fb = o resultado de uma função no ponto 'b' do intervalo [a,b]
+
+	Retorno: boolean
+	'''
+	if fa * fb < 0:
+		return True
+	else:
+		return False
+
+def biseccao(intervalo, condParada):
+	'''
+	Metodo usado para encontrar o ponto, aproximado, da raiz de uma função polinomial -> f(x) ~= 0.0
+
+	Parametros: \n
+	intervalo = Intervalo [a,b] onde se encontra a raiz da função \n
+	condParada = Valor que satisfaça a aproximação do valor da raiz ex: 0,001 ou 10**-3
+
+	Retorno: Ponto aproximado onde esta a raiz / valor da aproximação da raiz. -> f(x) ~= 0
+	'''
+	
 	# numero aproximado de iterações
 	k = (log10(intervalo[1] - intervalo[0])	- log10(condParada)) / log10(2)
 	
 	a = intervalo[0]
 	b = intervalo[1]	
+
+	if verificaSeTemRaiz(_fa(a), _fb(b)):
+		print("Intervalo = [{}, {}]".format(a,b))
+		print('-' * 50)
+		#ceil(k) Arredonda pra cima o numero de iterações k
+		for i in range(ceil(k)):
+
+			#xn = ponto Medio do intervalo, subistituindo o x da função principal
+			xn = (a+b)/2
+
+			# Verifica se a função com o ponto medio f(xn) ja é a raiz
+			if _fxn(xn) == 0:
+				return "Raiz = ", xn
+
+			# novo intervalo dividido ao meio dependendo da condição. [a, xn] ou [xn, b]
+			elif verificaSeTemRaiz(_fa(a), _fxn(xn)):
+				b = xn							
+			elif verificaSeTemRaiz(_fb(b), _fxn(xn)):
+				a = xn
+			
+			print("{} iteração: [{}, {}]".format(i,a,b))
+
+		# Enquanto o resultado aproximado da raiz da função f(xn) for negativo ou maior que a condição de parada, continua iterando
+		while(_fxn(xn) < 0 or _fxn(xn) > condParada):			
+			#xn = ponto Medio do intervalo, subistituindo o x da função principal
+			xn = (a+b)/2			
+			
+			# Verifica se a função com o ponto medio f(xn) ja é a raiz
+			if _fxn(xn) == 0:
+				return "Raiz = ", xn
+
+			# novo intervalo dividido ao meio dependendo da condição. [a, xn] ou [xn, b]
+			elif verificaSeTemRaiz(_fa(a), _fxn(xn)):
+				b = xn							
+			elif verificaSeTemRaiz(_fb(b), _fxn(xn)):
+				a = xn
+			
+			print("iteração extra: [{:.6f}, {:.6f}]".format(a,b))
+		
+		print('-' * 50)
+		print("Ponto no eixo x = ", b)
+		print('-' * 50)
+		print("Valor aproximado da raiz nesse ponto = ", _fxn(xn))
+		return b, _fxn(xn)
+	else:
+		return print("Não tem raiz no intervalo[{},{}]".format(a, b))
 	
-	print("Intervalo = [{}, {}]".format(a,b))
 
-	#Arredonda pra cima o numero de iterações k
-	for i in range(ceil(k)):
-		#resultado da função no intervalo [a,b], subistituindo o x da função principal
-		fa = a**3 - a + 1	
-		fb = b**3 - b + 1
-		
-		#xn = ponto Medio do intervalo, subistituindo o x da função principal
-		xn = (a+b)/2
-		fxn = xn**3 - xn + 1
-		
-		# novo intervalo dividido ao meio dependendo da condição. [a, xn] ou [xn, b]
-		if fa * fxn < 0:
-			b = xn
-						
-		if fb * fxn < 0:
-			a = xn
-		
-		print("[{}, {}]".format(a,b))
-	return b, fxn
-
-print("Ponto da raiz aproximada e valor = ", biseccao([-2,-1], 10**-6))
+biseccao([0,1], 0.001)
